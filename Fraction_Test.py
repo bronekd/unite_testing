1c41d69a306a28dd0cb518a490d6cf7a8163dfea
Bronislav Dřímal <bronislavdrimal@email.cz>
2024-05-21 21:18:41 +0200

    konec a popis DU

--- Fraction_Test.py
+++ Fraction_Test.py
@@ -12,10 +12,13 @@ class TestFraction(unittest.TestCase):
         self.assertEqual(f2, Fraction(1, 1))
 
     def test_str(self):
-        pass
+        self.assertEqual(str(Fraction(1, 1)), '1/1')
 
     def test_add(self):
         pass
 
 if __name__ == '__main__':
     unittest.main()
+
+#ukol task 2 je lepší umí věci zapsat a přečíst bude umět vrátit hodnoty v různých soustávách poskládat třádu a napsat testy. otestovat to jak převádět mezi octa, hexadecimal, binar
+# 
